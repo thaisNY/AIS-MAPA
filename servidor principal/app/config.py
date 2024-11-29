878ddcfd2f90ad94f764a5f0dc51f6d7941ecadc
João Pedro Medeiros <86727209+JoaoPedro-Medeiros@users.noreply.github.com>
2024-11-29 00:11:24 -0300

    Delete servidor principal directory

--- servidor principal/app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- servidor principal/app/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.routes.register_restaurant import register_restaurant_bp
-
-def init_routes(app):
-    app.register_blueprint(register_restaurant_bp, url_prefix='/register_restaurants')
