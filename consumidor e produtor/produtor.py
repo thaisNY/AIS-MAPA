6883ca0c5ddc810fbb051dbb8237dcfb1ce07781
Thais Rodrigues <71556315+thaisNY@users.noreply.github.com>
2024-12-02 07:35:02 -0300

    Add files via upload

--- consumidor e produtor/produtor.py
+++ consumidor e produtor/produtor.py
@@ -1,29 +1,48 @@
 import pika
 import json
 
-MQ_HOST = 'seu-broker.mq.us-east-1.amazonaws.com'  # Substitua pelo endereço do MQ
-MQ_USERNAME = 'seu_usuario'
-MQ_PASSWORD = 'sua_senha'
+# Configurações do Amazon MQ
+MQ_HOST = 'b-43a58687-c7df-40e3-9f81-398b4a8572cf-1.mq.us-east-1.amazonaws.com'
+MQ_PORT = 5671
+MQ_USERNAME = 'meu_usuario'
+MQ_PASSWORD = 'minha_senha1'
 
-def send_message(latitude, longitude, max_distance):
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            host=MQ_HOST,
-            credentials=pika.PlainCredentials(MQ_USERNAME, MQ_PASSWORD),
-            ssl=True
+class Consumer:
+    def __init__(self, queue_name='restaurant_data'):
+        self.queue_name = queue_name
+        self.connection = None
+        self.channel = None
+
+    def connect(self):
+        """Estabelece conexão com o Amazon MQ."""
+        credentials = pika.PlainCredentials(MQ_USERNAME, MQ_PASSWORD)
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host=MQ_HOST,
+                port=MQ_PORT,
+                credentials=credentials,
+                ssl_options=pika.SSLOptions()
+            )
         )
-    )
-    channel = connection.channel()
-    channel.queue_declare(queue='search_requests')
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue_name)
+        print(f"Conectado ao Amazon MQ e aguardando mensagens na fila '{self.queue_name}'")
 
-    message = {
-        'latitude': latitude,
-        'longitude': longitude,
-        'max_distance': max_distance
-    }
-    channel.basic_publish(exchange='', routing_key='search_requests', body=json.dumps(message))
-    print("Mensagem enviada:", message)
-    connection.close()
+    def start_consuming(self, callback):
+        """Inicia o consumo de mensagens."""
+        if not self.channel:
+            raise Exception("Conexão não estabelecida. Chame 'connect' antes de consumir mensagens.")
 
-# Teste enviando uma mensagem
-send_message(-23.563210, -46.654250, 5)
+        def on_message(ch, method, properties, body):
+            message = json.loads(body)
+            callback(message)  # Passa a mensagem para o callback externo
+
+        self.channel.basic_consume(queue=self.queue_name, on_message_callback=on_message, auto_ack=True)
+        print("Iniciando o consumo de mensagens...")
+        self.channel.start_consuming()
+
+    def close(self):
+        """Fecha a conexão com o Amazon MQ."""
+        if self.connection:
+            self.connection.close()
+            print("Conexão encerrada com o Amazon MQ.")
